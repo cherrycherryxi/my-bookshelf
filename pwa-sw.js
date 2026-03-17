9f1af2923da062db6d3c5b26e134d32c4b3e2780
huangnanxi <85931236+cherrycherryxi@users.noreply.github.com>
2026-03-17 21:33:32 +0800

    update

--- pwa-sw.js
+++ pwa-sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'reading-log-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const FILES = ['/my-bookshelf/', '/my-bookshelf/index.html', '/my-bookshelf/manifest.json', '/my-bookshelf/icon-192.png', '/my-bookshelf/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
